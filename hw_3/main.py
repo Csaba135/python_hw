724fd78f20561e7ef124400863fa9daf6e0a5ce9
Csaba135 <csabaharasztosi135@gmail.com>
2022-05-04 21:22:51 +0300

    ..

--- hw_3/main.py
+++ hw_3/main.py
@@ -4,9 +4,9 @@ import os
 from cars_filter import *
 if __name__ == "__main__":
     def writer_function(path,type_car):
-        with open(path, 'w') as json_file:
+        with open(path,"w") as json_file:
             json.dump(type_car, json_file, indent=2)
-    writer_function('output_data/slow_cars.json',slow_cars)
+    writer_function("output_data/slow_cars.json",slow_cars)
     writer_function('output_data/fast_cars.json', fast_cars)
     writer_function('output_data/sport_cars.json', sport_cars)
     writer_function('output_data/cheap_cars.json', cheap_cars)
--- hw_5.py
+++ hw_5.py
@@ -1,17 +1,22 @@
+from math import gcd,lcm
 class Fraction:
-    def __init__(self, numarator, numitor):
-        self.numarator = numarator
-        self.numitor = numitor
-    def __str__(self):
-        return "numarator/numitor"
-    def __add__(self):
-        return self.numitor+self.numarator
-    def __sub__(self):
-        return self.numarator-self.numitor
-    def invert(self):
-        return f'{self.numitor} {self.numarator}'
+    def __init__(self, numerator, denominator):
+        gcd_num=gcd(numerator, denominator)
+        lcm_num=lcm(numerator,denominator)
+        self.numerator = numerator//gcd_num
+        self.denominator = denominator//gcd_num
 
-a=Fraction(5,6)
-print(a.__add__())
-print(a.__sub__())
-print(a.invert())
+    def __add__(self, other):
+        new_numerator=self.numerator*other.denominator+other.numerator*self.denominator
+        new_denominator=self.denominator*other.denominator
+        return Fraction(new_numerator,new_denominator)
+    def __str__(self):
+        return f'{self.numerator}/{self.denominator}'
+
+fraction_1 = Fraction(5,18)
+fraction_2 = Fraction(2,3)
+fraction_3 = fraction_1+fraction_2
+print(fraction_1)
+print(fraction_2)
+print(fraction_3)
+
